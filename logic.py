52f5bcf1447a0323c32ff2a22244b2a9070b682f
Michael Cooney <MC509119@ohio.edu>
2020-06-12 16:14:21 -0400

    figuring out how to create a class

--- logic.py
+++ logic.py
@@ -6,3 +6,17 @@
 ## Description: logic files for minesweeper game
 ##
 ##//////////////////////////////////////////////////////////////////////////////
+
+class Empty:
+    #counter variable, getNumberBombs
+    def __init__(self):
+        self = self
+        
+    def sayHi():
+        print("HI")
+
+#class Bomb:
+
+if __name__ == "__main__":
+    x = Empty
+    x.sayHi()
