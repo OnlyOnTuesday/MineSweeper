39fab637808770ee78666a5ad4f00b914ecb9eb8
Michael Cooney <MC509119@ohio.edu>
2020-06-22 23:12:16 -0400

    Added comments to code

--- pygamePractice.py
+++ pygamePractice.py
@@ -7,13 +7,23 @@
 ##
 ##//////////////////////////////////////////////////////////////////////////////
 
+#the second line is optional but adds some constants and functions that wouldn't
+#be in the script otherwise (not sure what though yet)
 import pygame
 from pygame.locals import *
 
+#prepare all modules to be used.  Each can be inited individually too
 pygame.init()
 
+#create the surface that will be seen on the screen
+#this function should have a parameter that determines the window size
 disp = pygame.display.set_mode()
+
+#create a rectangle
+#I need to figure out what the first two variable do. Are they coordinates?
 r = pygame.Rect(10,20,30,40)
 pygame.draw.rect(disp, 102, r)
+
+#display the rectangle until the user stops python
 while 1:
     pygame.display.update()
