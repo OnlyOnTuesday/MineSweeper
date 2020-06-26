2b2745acec21ff43fdf3dd3c1055b7fcac27644a
Keit Vu <keitvu@Keits-MacBook-Pro.local>
2020-06-26 16:16:38 -0400

    screen

--- pygamePract.py
+++ pygamePract.py
@@ -1,1 +1,23 @@
+import pygame
+import sys
+from pygame.locals import *
 
+#initalize pygame
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+
+while True:
+    pass
+
+#class Bomb:
+
+#    def __init__(self, shape, color, image):
+#    self.shape = shape
+#        self.color = color
+#        self.image = image
+#
+#    def image(self):
+#        pygame.image.load("bombmine.png")
+
+#    def shape(self, surface)
